390251fe49b82713fb515e5029d98897a8b452f2
Jayakrishnan.p.v <jayakrishnan.pv@innogenio.com>
2025-11-01 20:47:31 +0530

    fix: added spinner

---
+++ src/app/[locale]/loading.tsx
@@ -0,0 +1,9 @@
+import Spinner from '@/components/atoms/Spinner/Spinner';
+
+export default function Loading() {
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center">
+      <Spinner />
+    </div>
+  );
+}
